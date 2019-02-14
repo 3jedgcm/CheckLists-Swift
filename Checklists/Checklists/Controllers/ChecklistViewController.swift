42fcc58ad701f3147469b0b2c6dbd8a244ac2efb
3jedgcm <clement.merlet@etu.univ-lyon1.fr>
2019-02-14 11:50:02 +0100

    Etape 10

--- Checklists/Checklists/Controllers/ChecklistViewController.swift
+++ Checklists/Checklists/Controllers/ChecklistViewController.swift
@@ -10,15 +10,38 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
 
+    var itemList: [CheckListItem] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        itemList.append(CheckListItem(text: "Cookie"))
+        itemList.append(CheckListItem(text: "Mashmallow"))
+        itemList.append(CheckListItem(text: "Candy"))
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, withItem item: CheckListItem)
+    {
+        if(item.checked)
+        {
+            cell.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+        else
+        {
+            cell.accessoryType = UITableViewCell.AccessoryType.none
+        }
+    }
+    
+    func configureText(for cell: UITableViewCell, withItem item: CheckListItem)
+    {
+        cell.textLabel?.text = item.text
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 1;
+        return itemList.count;
     }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -26,8 +49,10 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        cell.textLabel?.text = "Martin le genie"
+        var cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        self.configureCheckmark(for:cell, withItem: itemList[indexPath.row])
+        self.configureText(for:cell, withItem: itemList[indexPath.row])
+        
         return cell
     }
 
