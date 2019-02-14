4c745ee8af68fda91d354db052b8a021ee04220b
3jedgcm <clement.merlet@etu.univ-lyon1.fr>
2019-02-14 11:34:07 +0100

    Etape 8

---
+++ Checklists/Checklists/Models/ChecklistItem.swift
@@ -0,0 +1,25 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by lpiem on 14/02/2019.
+//  Copyright © 2019 Clément MERLET. All rights reserved.
+//
+
+import Foundation
+
+class CheckListItem {
+    var text = "";
+    var checked = false;
+    
+    init(text:String)
+    {
+        self.text = text;
+    }
+    
+    init(text:String, checked:Bool)
+    {
+        self.text = text;
+        self.checked = checked;
+    }
+}
