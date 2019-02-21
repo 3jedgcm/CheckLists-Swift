e6e2ef95d9950f5ffd5316f6c05c56e7b0d5cba9
3jedgcm <clement.merlet@etu.univ-lyon1.fr>
2019-02-21 11:26:18 +0100

    Etape 32

---
+++ Checklists/Checklists/Controllers/ChecklistItemCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChecklistItemCellTableViewCell.swift
+//  Checklists
+//
+//  Created by lpiem on 21/02/2019.
+//  Copyright © 2019 Clément MERLET. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistItemCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
